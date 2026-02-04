1661c65954a1e89316c9654d0d99a1fa3139ee76
JocsanCantera <jocsan.cantera@gmail.com>
2026-02-03 19:00:25 -0600

    feat: ejemplos if

--- condicionales.js
+++ condicionales.js
@@ -27,3 +27,24 @@ if(edad >= 18 && tieneINE){
 }else{
     console.log("No puedes votar")
 }
+
+let numero = prompt("Ingresa un numero:")
+
+numero = parseInt(numero);
+
+// Si es divisible o no entre 2
+if(numero % 2 === 0){
+    console.log("Es divisible entre 2")
+}else{
+    console.log("No es divisible entre 2")
+}
+
+let esPremium = true;
+let totalDeCompra = 350;
+let esFinDeSemana = false;
+
+if(esPremium || (totalDeCompra > 1000 && esFinDeSemana)){
+    console.log("Descuento aplicado")
+}else{
+    console.log("No aplica el descuento")
+}
