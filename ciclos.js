2174e2198791e8d20eb0ab7f006935b026e05d8c
JocsanCantera <jocsan.cantera@gmail.com>
2026-02-03 20:01:04 -0600

    feat: ciclos

--- ciclos.js
+++ ciclos.js
@@ -23,3 +23,38 @@ console.log(frutas.length);
 for(let indice = 0; indice < frutas.length; indice++){
     console.log("Fruta =", frutas[indice])
 }
+
+
+console.log("Metodos de los arreglos")
+let alumno = []; //arreglo vacio
+
+// metodo push agrega al final
+alumno.push('Ana');
+alumno.push('Bruno');
+alumno.push('Fernanda');
+alumno.push('Natalia');
+
+console.log(alumno)
+
+//metodo pop quita el ultimo
+alumno.pop();
+
+console.log(alumno);
+
+// metodo unshift agrega al inicio
+alumno.unshift('Nina');
+alumno.unshift('Misael');
+alumno.unshift('Beto');
+console.log(alumno);
+
+//metodo shift quita al principio
+alumno.shift();
+console.log(alumno);
+
+
+// arreglo de una lista de super
+// Impriman lista del super
+// se imprima en indice (1 Manzana, 2 Jabon)
+
+// Extra total de productos
+// Extra promt (guardar la lista que da el usuario dentro de un arreglo)
